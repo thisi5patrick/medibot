4e40d3c05b7b550d4c2c71d6430ded3092fa4551
thisi5patrick <patrykbielen97@gmail.com>
2024-12-27 11:18:28 +0100

    Added error handler

--- src/telegram_interface/bot.py
+++ src/telegram_interface/bot.py
@@ -1,15 +1,16 @@
 import logging
 import os
 from pathlib import Path
-from typing import Any
+from typing import Any, cast
 
 from dotenv import load_dotenv
-from telegram import BotCommand
+from telegram import BotCommand, Chat, Update
 from telegram.ext import (
     Application,
     ApplicationBuilder,
     CallbackQueryHandler,
     CommandHandler,
+    ContextTypes,
     ConversationHandler,
     MessageHandler,
     PicklePersistence,
@@ -99,6 +100,31 @@ async def post_init(application: Application[Any, Any, Any, Any, Any, Any]) -> N
             BotCommand("/settings", "Change the bot settings"),
             BotCommand("/help", "Show help message"),
         ]
+    )
+
+
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.error("Exception while handling an update:", exc_info=context.error)
+
+    update = cast(Update, update)
+    chat = cast(Chat, update.effective_chat)
+    chat_id = chat.id
+
+    if isinstance(context.error, KeyError):
+        missing_key = context.error.args[0]
+        if missing_key == "language":
+            logger.warning("KeyError: Missing 'language' key in user data.")
+            await context.bot.send_message(
+                chat_id=chat_id,
+                text="The bot has not been initialized properly. Please run the /start command.",
+            )
+            return None
+        else:
+            logger.warning("KeyError: An error has occurred. Missing '%s' key in user data.", missing_key)
+
+    await context.bot.send_message(
+        chat_id=chat_id,
+        text="Something went wrong. Please try again later.",
     )
 
 
@@ -301,12 +327,16 @@ class TelegramBot:
             allow_reentry=True,
         )
 
+        logger.info("Starting Telegram bot")
+
         self.bot.add_handler(start_handler, 0)
         self.bot.add_handler(login_handler, 1)
         self.bot.add_handler(new_monitoring_handler, 2)
         self.bot.add_handler(active_monitorings, 3)
         self.bot.add_handler(settings_handler, 4)
         self.bot.add_handler(future_appointments_handler, 5)
+
+        self.bot.add_error_handler(error_handler)
 
         self.bot.run_polling()
 
