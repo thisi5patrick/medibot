2c068c963b1b4c74b3570c6ede2f363ea16f9a29
thisi5patrick <patrykbielen97@gmail.com>
2024-12-27 11:33:33 +0100

    Fixed error handler

--- src/telegram_interface/bot.py
+++ src/telegram_interface/bot.py
@@ -1,10 +1,14 @@
+import html
+import json
 import logging
 import os
+import traceback
 from pathlib import Path
 from typing import Any, cast
 
 from dotenv import load_dotenv
 from telegram import BotCommand, Chat, Update
+from telegram.constants import ParseMode
 from telegram.ext import (
     Application,
     ApplicationBuilder,
@@ -106,6 +110,26 @@ async def post_init(application: Application[Any, Any, Any, Any, Any, Any]) -> N
 async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
     logger.error("Exception while handling an update:", exc_info=context.error)
 
+    if update is None:
+        error = cast(Exception, context.error)
+        tb_list = traceback.format_exception(None, error, error.__traceback__)
+        tb_string = "".join(tb_list)
+
+        update_str = str(update)
+
+        message = (
+            "An exception was raised while handling an update\n"
+            f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+            "</pre>\n\n"
+            f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+            f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+            f"<pre>{html.escape(tb_string)}</pre>"
+        )
+        await context.bot.send_message(
+            chat_id=os.environ["TELEGRAM_ADMIN_CHAT_ID"],
+            text=message,
+            parse_mode=ParseMode.HTML,
+        )
     update = cast(Update, update)
     chat = cast(Chat, update.effective_chat)
     chat_id = chat.id
